2727046c2f30dcf7775f83e5c13964c73e99182f
Darrion Harris <Darrion.Harris.ext@bmwiti.onmicrosoft.com>
2018-10-16 15:05:28 -0700

    rollback prod to error state instead of preprod

--- java/spring/container/Scripts/rollback-groups-by-tag.sh
+++ java/spring/container/Scripts/rollback-groups-by-tag.sh
@@ -4,7 +4,7 @@ groups=$(az group list --tag Environment=prod --query "[].name" --output tsv)
 for name in $groups
 do
 	echo "rolling back tags for resource group:" $name
-	az group update --name $name --tags Environment=preprod
+	az group update --name $name --tags Environment=error
 done
 echo "Querying for depreciated resource groups"
 groups=$(az group list --tag Environment=depreciated --query "[].name" --output tsv)
