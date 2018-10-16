57d83e1399ada8f9d33a1a528de994866464a221
Darrion Harris <Darrion.Harris.ext@bmwiti.onmicrosoft.com>
2018-10-16 11:42:58 -0700

    create script for deleting resource groups by tag

---
+++ java/spring/container/Scripts/delete-groups-by-tag.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+tag=$1
+chrlen=${#tag}
+echo "param length:" $chrlen
+if [ $chrlen -ge 4 ]; then
+    echo "Querying for production resource groups tagged with:" $tag
+    groups=$(az group list --tag $tag --query "[].name" --output tsv)
+    for name in $groups
+    do
+        echo "Deleting resource group:" $name
+        az group delete --name $name -y
+    done
+else
+    echo "Please enter a tag name with 4 or more characters"
+fi
