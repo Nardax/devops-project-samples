ea84c334f1ae14ab007e0177ec0df8170d3f60e7
Darrion Harris <Darrion.Harris.ext@bmwiti.onmicrosoft.com>
2018-10-16 12:53:21 -0700

    creat3e script to rollback resource group environment tags.

---
+++ java/spring/container/Scripts/rollback-groups-by-tag.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+echo "Querying for production resource groups"
+groups=$(az group list --tag Environment=prod --query "[].name" --output tsv)
+for name in $groups
+do
+	echo "rolling back tags for resource group:" $name
+	az group update --name $name --tags Environment=preprod
+done
+echo "Querying for depreciated resource groups"
+groups=$(az group list --tag Environment=depreciated --query "[].name" --output tsv)
+for name in $groups
+do
+	echo "rolling back tags for resource group:" $name
+	az group update --name $name --tags Environment=prod
+done
