36c877dc0fa54e9758acbbe620bce74f50359ac2
Anna Rode <anrode@microsoft.com>
2018-10-22 17:56:58 -0700

    updating scripts

--- armtemplates/createDeployment.sh
+++ armtemplates/createDeployment.sh
@@ -1,18 +1,48 @@
 #!/bin/bash
 # Copyright (c) Microsoft Corporation. All rights reserved.
 
+RESOURCE_GROUP="ResourceGroupName"
+WORKSPACE_NAME="WorkspaceName"
+AG_NAME="YourActionGroupName"
+AG_SHORTNAME="AGShortName"
+AG_EMAIL_NAME="myEmailName"
+AG_EMAIL="myEmail@gmail.com"
+ALERT_NAME="CPUAlert"
+
+# Create resource group
+az group create -l westus -n ${RESOURCE_GROUP}
+
+# Deploy workspace
 cd workspace
+az group deployment create --resource-group ${RESOURCE_GROUP} \
+    --name azuredeploy_workspace --template-file ./azuredeploy.json \
+    --parameters azuredeploy.parameters.json \
+    --parameters workspaceName=${WORKSPACE_NAME} \
 
-az group deployment create --resource-group aksdevsandbox3 --name azuredeploy_workspace --template-file ./azuredeploy.json --parameters azuredeploy.parameters.json
-
+# Deploy action group
 cd ../actionGroup
+az group deployment create --resource-group ${RESOURCE_GROUP} \
+    --name azuredeploy_actiongroup --template-file ./azuredeploy.json \
+    --parameters azuredeploy.parameters.json \
+    --parameters actiongroupName=${AG_NAME} \
+    --parameters actionGroupShortName=${AG_SHORTNAME} \
+    --parameters emailName=${AG_EMAIL_NAME} \
+    --parameters email=${AG_EMAIL}
 
-az group deployment create --resource-group aksdevsandbox3 --name azuredeploy_actiongroup --template-file ./azuredeploy.json --parameters azuredeploy.parameters.json
-
+# Deploy alerts
 cd ../alerts
+az group deployment create --resource-group ${RESOURCE_GROUP} \
+--name azuredeploy_alerts --template-file ./azuredeploy.json \
+--parameters azuredeploy.parameters.json \
+--parameters workspaceName=${WORKSPACE_NAME} \
+--parameters actiongroupName=${AG_NAME} \
+--parameters alertName=${ALERT_NAME}
 
-az group deployment create --resource-group aksdevsandbox3 --name azuredeploy_alerts --template-file ./azuredeploy.json --parameters azuredeploy.parameters.json
-
+# Hook VMs to workspace
 cd ../workSpaceVMAgent
-
-./VMAgentHook.sh "azuredeploy_workspace"
+./VMAgentHook.sh \
+    "clusterresourcegroupname" \
+    "${RESOURCE_GROUP}" \
+    "westus2" \
+    "clustername" \
+    "azuredeploy_workspace" 
